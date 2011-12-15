0258775a80f46723d20d9afe1ac0a51d6c264597
Tjark Saul <sauli1991@gmail.com>
2011-12-15 01:39:33 +0100

    iteration through all mensae in Hamburg

--- extract.php
+++ extract.php
@@ -1,46 +1,52 @@
 <?php
 	require_once 'simple_html_dom.php';
+	require_once 'mensen.php';
 	
 	$week = date('W');
-	$html = file_get_html('http://www.studierendenwerk-hamburg.de/essen/' 
-			. 'woche.php?haus=Philosophenturm&&kw=' . $week);
-	$essenTable = $html->find('table', 1);
-	
 	$essen = array();
-	for ($k=1;$k<=5;$k++)
+	
+	foreach ($mensen as $mensa => $url)
 	{
-		$essen[$k] = array();
-	}
-	$i = 0;
-	foreach($essenTable->find('tr') as $tr)
-	{
-		$i++;
-		if ($i < 3) 
-		{
-			continue;
-		}
+		$html = file_get_html('http://www.studierendenwerk-hamburg.de/essen/' 
+				. 'woche.php?haus=' . $url . '&&kw=' . $week);
+		$essenTable = $html->find('table', 1);
 		
-		$j = 0;
-		foreach ($tr->find('td') as $td)
+		$essen[$mensa] = array();
+		for ($k=1;$k<=5;$k++)
 		{
-			if ($j == 0) 
+			$essen[$k] = array();
+		}
+		$i = 0;
+		foreach($essenTable->find('tr') as $tr)
+		{
+			$i++;
+			if ($i < 3) 
 			{
-				for ($k=1;$k<=5;$k++)
+				continue;
+			}
+			
+			$j = 0;
+			foreach ($tr->find('td') as $td)
+			{
+				if ($j == 0) 
 				{
-					$essen[$k][$i] = array();
-					$essen[$k][$i]['type'] = $td->plaintext;
+					for ($k=1;$k<=5;$k++)
+					{
+						$essen[$mensa][$k][$i] = array();
+						$essen[$mensa][$k][$i]['type'] = $td->plaintext;
+					}
 				}
+				else
+				{
+					$temp = str_replace("<br>","\n",$td->innertext);
+					$temp = str_replace("<img src=\"images/3.gif\">","\n(mit Schweinefleisch)",$temp);
+					$temp = str_replace("<img src=\"images/2.gif\">","\n(mit Alkohol)",$temp);
+					$temp = str_replace("<img src=\"images/1.gif\">","\n(fleischloses Gericht)",$temp);
+					$temp = strip_tags($temp);
+					$essen[$mensa][$j][$i]['essen'] = $temp;
+				}
+				$j++;
 			}
-			else
-			{
-				$temp = str_replace("<br>","\n",$td->innertext);
-				$temp = str_replace("<img src=\"images/3.gif\">","\n(mit Schweinefleisch)",$temp);
-				$temp = str_replace("<img src=\"images/2.gif\">","\n(mit Alkohol)",$temp);
-				$temp = str_replace("<img src=\"images/1.gif\">","\n(fleischloses Gericht)",$temp);
-				$temp = strip_tags($temp);
-				$essen[$j][$i]['essen'] = $temp;
-			}
-			$j++;
 		}
 	}
 ?>
