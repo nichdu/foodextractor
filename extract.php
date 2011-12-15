a1db1917f9d2152d7a91d90679c398f92b4c0285
Tjark Saul <sauli1991@gmail.com>
2011-12-15 01:05:36 +0100

    added basic code

--- extract.php
+++ extract.php
@@ -4,6 +4,36 @@
 	$html = file_get_html('http://www.studierendenwerk-hamburg.de/essen/woche.php?haus=Philosophenturm&&kw=50');
 	$essenTable = $html->find('table', 1);
 	
-	echo '<pre>';
-	print_r($essenTable);
+	$essen = array();
+	for ($k=1;$k<=5;$k++)
+	{
+		$essen[$k] = array();
+	}
+	$i = 0;
+	foreach($essenTable->find('tr') as $tr)
+	{
+		$i++;
+		if ($i < 3) 
+		{
+			continue;
+		}
+		
+		$j = 0;
+		foreach ($tr->find('td') as $td)
+		{
+			if ($j == 0) 
+			{
+				for ($k=1;$k<=5;$k++)
+				{
+					$essen[$k][$i] = array();
+					$essen[$k][$i]['type'] = $td->plaintext;
+				}
+			}
+			else
+			{
+				$essen[$j][$i]['essen'] = strip_tags(str_replace("<br>","\n",$td->innertext));
+			}
+			$j++;
+		}
+	}
 ?>
