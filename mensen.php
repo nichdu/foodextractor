96e6e152b824f629135c7c066b56fc3f5f277c6d
Tjark Saul <sauli1991@gmail.com>
2011-12-15 01:36:38 +0100

    added list of mensae

---
+++ mensen.php
@@ -0,0 +1,19 @@
+<?php
+	$mensen = array();
+	$mensen['Armgartstraße'] = 'Armgartstra%DFe';
+	$mensen['Bergedorf'] = 'Bergedorf';
+	$mensen['Berliner Tor'] = 'Berliner%20Tor';
+	$mensen['Bistro Averhoffstraße'] = 'Bistro%20Averhoffstra%DFe';
+	$mensen['Botanischer Garten'] = 'Botanischer%20Garten';
+	$mensen['Bucerius Law School'] = 'Bucerius%20Law%20School';
+	$mensen['Café Alexanderstraße'] = 'Caf%E9$20Alexanderstra%DFe';
+	$mensen['Café Jungiusstraße'] = 'Caf%E9%20Jungiusstra%DFe';
+	$mensen['Campus'] = 'Campus';
+	$mensen['City Nord'] = 'City%20Nord';
+	$mensen['Finkenau'] = 'Finkenau';
+	$mensen['Geomatikum'] = 'Geomatikum';
+	$mensen['Harburg'] = 'Harburg';
+	$mensen['Philosophenturm'] = 'Philosophenturm';
+	$mensen['Stellingen'] = 'Stellingen';
+	$mensen['Studierendenhaus'] = 'Studierendenhaus';
+?>
